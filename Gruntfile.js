a733e9bd33cdd60724f67e6cc792e2e39bc23fb4
Jens Doose <jens.doose@onwerk.de>
2016-06-01 16:08:48 +0200

    Adding gruntfile for jshint

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                ignores: []
+            },
+            files: {
+                src: [
+                'bin/**/*.js',
+                'lib/**/*.js']
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            }
+        }
+        
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Default task(s).
+
+    grunt.registerTask('default', ['jshint']);
+};
